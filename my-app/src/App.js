6e202991e9ac82041ae0ae30d4c145a51f93f3dd
dankla90 <bassboom@hotmail.com>
2023-12-01 13:59:41 +0100

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here is where the encounter generator will be displayed
         </p>
         <a
           className="App-link"

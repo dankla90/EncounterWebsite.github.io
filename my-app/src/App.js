5b0b62afdd5f6c503e445bf249309283831fe284
dankla90 <bassboom@hotmail.com>
2023-12-01 18:31:48 +0100

    started, barely

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import Encounter from './EncounterGenerator';
+// Note to self to deploy run: npm run deploy 
+
+
 
 function App() {
   return (
@@ -9,13 +13,14 @@ function App() {
         <p>
           Here is where the encounter generator will be displayed
         </p>
+        <div><Encounter></Encounter></div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/dankla90/DnDEncounterGenerator"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is the project I am wanting to recreate, just on a website
         </a>
       </header>
     </div>
---
+++ my-app/src/EncounterGenerator.tsx
@@ -0,0 +1,22 @@
+import { MouseEvent } from "react";
+
+// Here is the encounter generator, I might need to divide it up into sections, will see. 
+
+
+
+
+
+function Encounter() {
+    return (
+        <>       
+        <h1>Encounter</h1>
+        {}
+        <button>Make some encounters</button>
+        </>
+    );
+}
+
+export default Encounter;
+
+
+
